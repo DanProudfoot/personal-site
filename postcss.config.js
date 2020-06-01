7559b9f1403f8378e922d3cb95682007bc8e4b00
Dan Proudfoot <dnprdft@gmail.com>
2020-06-01 13:18:09 +0100

    Add clamp support???

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = () => ({
 	plugins: [
 		require('postcss-import'),
 		require('postcss-nested'),
+		require('postcss-clamp'),
 		require('postcss-preset-env')({
 			stage: 0,
 			importFrom: 'src/styles/main.css'
